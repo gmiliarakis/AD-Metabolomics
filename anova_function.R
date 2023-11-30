634cb6c2902e9ff2bc8dfd046b4e582090d9b36e
gmiliarakis <miliarakisg@icloud.com>
2023-11-30 21:06:55 +0100

    included other clinical confounders

--- anova_function.R
+++ anova_function.R
@@ -1,40 +1,46 @@
-library(caret)
 library(dplyr)
-# Transpose, standardize and store the metabolite data in Y
-Y <- scale(ADmetabolites)
+load("data.Rdata")
 
-# Store the ApoE genotype in x as a factor
-x <- as.factor(geno$APOE)
-sex <- as.factor(geno$sex)
-df <- cbind.data.frame(Y,x)
 
-gentest <- function(Y, x, sex= NULL) {
+library(car)
+gentest <- function(Y, x, ...) {
   ### Analysis of Variance (ANOVA)
-  # Store the ANOVA model summaries in summaries
-  if (is.null(sex)){
-    df <- cbind.data.frame(Y,x)
-    summaries <- purrr::map(df[,1:230], ~summary(aov(.x ~ df$x)))
-  } else {
-    df <- cbind.data.frame(Y,x,sex)
-    summaries <- purrr::map(df[,1:230], ~summary(aov(.x ~ df$x+df$x*df$sex)))}
+  df <- cbind(Y, x, ...)
+  ncol <- ncol(Y)
+  covariates <- setdiff(names(df), c(names(Y), x))
+  summaries <- purrr::map(df[, 1:ncol], ~ summary(lm(as.formula(paste0(".x ~ x +", paste(covariates, collapse = "*"))), data = df), test.statistic = "F", type = 3))
+
   ### Correction for Multiple Testing
-  #Create a list to store the p-values
-  p_values <- list(1:230)
-  #Extract the p-values of the F-tests from the aov summaries list and store them in p_values
-  for (i in 1:230){
-    p_values[[i]] <- summaries[[i]][[1]][["Pr(>F)"]][[1]]
-  }
-  # Coerce p_values to dataframe and transpose it 
-  p_values <- t(data.frame(p_values))
-  
-  # Set as row names the metabolites
-  p_values <- data.frame(p_values, row.names = colnames(Y))
-  
-  # Calculate the FDR-adjusted p-values
-  p_values$p_adj <- p.adjust(p_values[,], method = "hochberg",n=230)
-  
+  # Create a list to store the p-values
+  # p_values <- list(1:ncol)
+  # # Extract the p-values of the F-tests from the aov summaries list and store them in p_values
+  # for (i in 1:ncol) {
+  #   p_values[[i]] <- summaries[[i]][[1]][["Pr(>F)"]][[1]]
+  # }
+  # # Coerce p_values to dataframe and transpose it
+  # p_values <- t(data.frame(p_values))
+
+  # # Set as row names the metabolites
+  # p_values <- data.frame(p_values, row.names = colnames(Y))
+
+  # # Calculate the FDR-adjusted p-values
+  # p_values$p_adj <- p.adjust(p_values[, ], method = "fdr")
+  # out <- list(summaries, filter(p_values, p_adj < 0.05) )
   # Filter out the non-significant (a=0.05) FDR-adjusted p-values
-  return(filter(p_values, p_adj < 0.05))
+  return(summaries)
 }
-gentest(Y, x, sex=AD$I_sex)
-ADmetabolites
+
+Y <- df[, 1:230]
+df$Diagnosis <- as.numeric(df$Diagnosis)
+x <- as.(df$APOE)-1
+sex <- as.numeric(df$sex)-1
+D <- as.numeric(df$Diagnosis) -1
+
+gentest(Y, x, sex)
+
+Y <- subset(df, Diagnosis == 1)[,1:230]
+x <- as.factor(subset(df, Diagnosis == 1)$APOE)
+sex <- as.factor(subset(df, Diagnosis == 1)$sex)
+
+gentest(Y, x)
+install.packages("car")
